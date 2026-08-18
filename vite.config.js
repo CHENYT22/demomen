4a113c879d90f2faece65968cd5723604079fc91
unknown <cyt35316@gmail.com>
2026-08-18 16:13:54 +0800

    fix github pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './src/test/setup.js',
-  },
-});
+  base: '/demomen/',
+})
